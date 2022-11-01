3c10a4b7397df2f7ece7add580d245438605d1b1
Tomoyuki Fujii <mjr01300828@gmail.com>
2022-11-01 14:54:47 -0700

    Added some changes

--- src/Style/Introduce.js
+++ src/Style/Introduce.js
@@ -26,7 +26,7 @@ export const IntroduceStyle = styled.div`
     }
 
     .main_self_introduce_sentence {
-      padding: 0% 5%;
+      padding: 0% 10%;
       font-size: 16px;
       background-color: rgba(255, 255, 255, 0.35);
     }
